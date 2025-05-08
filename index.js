bc6da292a8e6239287202b11a3d4c0d1be6f493e
bblair321 <brady@BradysPC.>
2025-05-08 12:48:30 -0700

    passed all test

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+function logAttendeeName(attendee){
+  return console.log(attendee.name);
+}
 
+function logTicketPrice(attendee){
+  return console.log(attendee.ticketPrice);
+}
 
+function updateTicketType(attendee, newticketType){
+  attendee.ticketType = newticketType;
+}
+
+function removeEventProperty(attendee){
+  delete attendee.event;
+  return attendee;
+}
+
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true;
+  return attendee;
+}
+
+function updateTicketPrice(attendee, newticketPrice){
+  attendee.ticketPrice = newticketPrice;
+}
 //Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
